f3a49b3e8b9688eee798dd15544c4b8658c290d8
gadhiaeesha <gadhiaeesha@gmail.com>
2023-04-12 19:16:45 -0700

    fixing server.js

--- server.js
+++ server.js
@@ -10,8 +10,7 @@ const HOST = '0.0.0.0';
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200);  
-  res.json({ 
+  res.status(200).json({ 
         "message": "My name is Eesha Gadhia", 
         "timestamp" : new Date()
     });
